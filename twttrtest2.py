75a481316d3079b2f84c3a75ebb561e5380b5cb0
lavoieunh <khl1004@wildcats.unh.edu>
2018-05-07 17:44:43 -0400

    Update twttrtest2.py

--- twttrtest2.py
+++ twttrtest2.py
@@ -4,13 +4,13 @@ import requests
 import oauth2 as oauth
 #from requests_oauthlib import OAuth1, OAuth2
 
-API_KEY='455215929-Exmhl2w5l1LdckIkFWRoiahwmrawltGzwWQ1Mffs'
+API_KEY=''
 
-API_SECRET='elIv4IjcH9IuJNYUWwh7zbzsJJ83P5mIOEsqoQfaJ4Ri9'
+API_SECRET=''
 
-CONSUMER_KEY='LmsuQDk0UNVEk0BzXDEMXVyHw'
+CONSUMER_KEY=''
 
-CONSUMER_SECRET='J5QTV4bELgTH6PemoXeVkpw8y2AktHClcAVvUB27pM6NGAFbr3'
+CONSUMER_SECRET=''
 """
 consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
 access_token = oauth.Token(key=API_KEY, secret = API_SECRET)
